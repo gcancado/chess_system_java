d0ee1cbb563b46a8aba6ef390b1739e4462db3dc
Gabriel Cancado <gabriel.cancadoc@gmail.com>
2024-07-09 10:27:14 -0300

    Implementando movimentos possiveis do rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,63 @@ public class King extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        //acima da peca
+        p.setValues(this.position.getRow() - 1, this.position.getColumn());
+        if(this.getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //abaixo da peca
+        p.setValues(this.position.getRow() + 1, this.position.getColumn());
+        if(this.getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //A esqueda da peca
+        p.setValues(this.position.getRow(), this.position.getColumn() - 1);
+        if(this.getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //A direita da peca
+        p.setValues(this.position.getRow(), this.position.getColumn() + 1);
+        if(this.getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Diagonal esquerda cima
+        p.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+        if(this.getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Diagonal esquerda baixo
+        p.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+        if(this.getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Diagonal direita cima
+        p.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+        if(this.getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //Diagonal direita baixo
+        p.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+        if(this.getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
         return mat;
     }
+
+    public boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return (p == null) || p.getColor() != this.getColor();
+    }
+
 }
